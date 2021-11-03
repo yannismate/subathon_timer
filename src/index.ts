2293e7ed9a06e94a39f83e6991d6309edb900877
Yannis Matezki <git@yannismate.de>
2021-11-04 00:59:17 +0100

    Add sqlite and config

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,34 @@
 import * as express from "express";
 import * as http from "http";
 import * as socketio from "socket.io";
+import sqlite3 from 'sqlite3'
+import * as sqlite from 'sqlite'
+import cfg from '../config.json'
 
-const port = 8080;
+(async () => {
+  // open the database
+  const db = await sqlite.open({
+    filename: './data.db',
+    driver: sqlite3.Database
+  });
+  await db.open();
+  await db.run('CREATE TABLE IF NOT EXISTS subs (timestamp INTEGER, ending_at INTEGER, seconds_per_sub INTEGER, tier INTEGER, user_name TEXT);');
+  await db.run('CREATE TABLE IF NOT EXISTS sub_bombs (timestamp INTEGER, amount_subs INTEGER, tier INTEGER, user_name TEXT);');
+  await db.run('CREATE TABLE IF NOT EXISTS graph (timestamp INTEGER, ending_at INTEGER);');
 
-const app = express.default();
-app.use(express.static('public'));
+  const app = express.default();
+  app.use(express.static('public'));
 
-const server = http.createServer(app);
-const io = new socketio.Server(server);
-
-
-io.on('connection', (socket) => {
-  console.log(socket);
-});
+  const server = http.createServer(app);
+  const io = new socketio.Server(server);
 
 
-server.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
-});
+  io.on('connection', (socket) => {
+    console.log(socket);
+  });
+
+
+  server.listen(cfg.port, () => {
+    console.log(`server started at http://localhost:${cfg.port}`);
+  });
+})();

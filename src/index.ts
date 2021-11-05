5a7af0ac427f21e1634618204fab33265520ce78
Yannis Matezki <git@yannismate.de>
2021-11-05 11:48:49 +0100

    Add tmi.js

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as socketio from "socket.io";
 import sqlite3 from 'sqlite3'
 import * as sqlite from 'sqlite'
 import cfg from '../config.json'
+import * as tmi from 'tmi.js';
 
 (async () => {
   // open the database
@@ -15,6 +16,21 @@ import cfg from '../config.json'
   await db.run('CREATE TABLE IF NOT EXISTS subs (timestamp INTEGER, ending_at INTEGER, seconds_per_sub INTEGER, tier INTEGER, user_name TEXT);');
   await db.run('CREATE TABLE IF NOT EXISTS sub_bombs (timestamp INTEGER, amount_subs INTEGER, tier INTEGER, user_name TEXT);');
   await db.run('CREATE TABLE IF NOT EXISTS graph (timestamp INTEGER, ending_at INTEGER);');
+
+
+  const twitch = tmi.Client({
+    channels: [cfg.channel],
+    identity: cfg.twitch_token ? {
+      username: cfg.channel,
+      password: cfg.twitch_token
+    } : undefined
+  });
+  await twitch.connect().catch(console.error);
+
+  twitch.on('message', (channel: string, userstate: tmi.ChatUserstate, message: string ,self: boolean) => {
+    if(self) return;
+    console.log(message);
+  });
 
   const app = express.default();
   app.use(express.static('public'));
@@ -29,6 +45,6 @@ import cfg from '../config.json'
 
 
   server.listen(cfg.port, () => {
-    console.log(`server started at http://localhost:${cfg.port}`);
+    console.log(`Open the timer at http://localhost:${cfg.port}/timer.html`);
   });
 })();
